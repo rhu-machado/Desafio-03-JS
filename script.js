8e5fb22f56d4e5501b6c8bf42755631f089b79d8
rhu-machado <rhu.machado16@gmail.com>
2022-06-22 17:23:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let i 
+for (i = 1; i <= 10 ; i ++){
+    console.log(i)
+}
+
+i = 10
+while(i != 0){
+    console.log(i)
+    i --
+}
+
+/*  */
+
+let j
+for (j = 1;j<=100;j+=2){
+    console.log(j)
+}
+
+j = 0
+while(j < 100){
+    console.log(j)
+    j+=2
+}
